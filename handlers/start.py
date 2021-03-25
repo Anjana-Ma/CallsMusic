43cba33b10d5fad4e37fd9920fe7d2918a6005c4
Anjana Madushanka <71401231+Anjana-Ma@users.noreply.github.com>
2021-03-25 15:41:14 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,35 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.filters import command, other_filters, other_filters2
 
 
-@Client.on_message(command("start") & other_filters2)
-async def start(_, message: Message):
-    await message.reply_text(
-        f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
-
-I am Calls Music, an open-source bot that lets you play music in your groups.
-
-Use the buttons below to know more about me.""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "⚒ Source code", url="https://github.com/callsmusic/callsmusic"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/callsmusicchat"
-                    ),
-                    InlineKeyboardButton(
-                        "Channel 🔈", url="https://t.me/callsmusic"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_message(command("start") & other_filters)
+@Client.on_message(command("search") & other_filters)
 async def start2(_, message: Message):
     await message.reply_text(
         "💁🏻‍♂️ Do you want to search for a YouTube video?",
